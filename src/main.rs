4809bee237016e2f8b4c56cf252b972f0095babf
Alex <noelalexandre66@gmail.com>
2025-01-12 18:40:00 +0000

    Fix small thing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::fs::read_to_string;
 use anyhow::{anyhow, Error};
 use clap::Parser;
+use std::fs::read_to_string;
 mod cli;
 mod exec;
 mod parser;
@@ -8,11 +8,8 @@ mod parser;
 fn main() -> Result<(), Error> {
     let args = cli::Args::parse();
     let content = read_to_string(args.path).map_err(|e| anyhow!("Failed to read file: {}", e))?;
-    // If using map_err and ?, this won't compile: lifetime mismatch from desugared ? operator, so we'll go manual:
-    let instrs = match parser::parse_file(&content, args.prefixed) {
-        Ok(instrs) => instrs,
-        Err(e) => return Err(anyhow!("Failed to parse file: {:#?}", e)),
-    };
+    let instrs = parser::parse_file(&content, args.prefixed)
+        .map_err(|e| anyhow!("Failed to parse file: {:#?}", e))?;
 
     exec::execute(instrs).map_err(|e| anyhow!("Failed to execute program: {:#?}", e))?;
 
